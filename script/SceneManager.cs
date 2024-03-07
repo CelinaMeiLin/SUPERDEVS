b45f3fc8a1d2f063ede4ed330ff2c7fc900f0880
Célina Chau <celina.chau@epita.fr>
2024-03-07 12:21:59 +0100

    menu_audio

---
+++ script/SceneManager.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public enum eSceneNames {
+	
+	MainMenu = 10,
+	Tuto = 20
+}
+
+public partial class SceneManager : Node {
+
+	public static SceneManager instance;
+
+	public Dictionary<eSceneNames, SceneData> sceneDictionary = new Dictionary<eSceneNames, SceneData>() {
+		{eSceneNames.MainMenu, new SceneData("res://scene/main_menu.tscn", "main_menu", false) },
+		{eSceneNames.Tuto, new SceneData("res://scene/tuto.tscn", "tuto", true) },
+	};
+
+	public override void _Ready() {
+		instance = this;
+	}
+
+	public void ChangeScene(eSceneNames mySceneName)
+	{
+		string myPath = sceneDictionary[mySceneName].path;
+		GetTree().ChangeSceneToFile(myPath);
+	}
+
+}
