f2c0e615d5caacc6bd6591fdd918da5d3676f4a7
matthieu.greiner <matthieu.greiner@epita.fr>
2024-03-18 16:33:07 +0100

    ekekeke

---
+++ options_multi.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class options_multi : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void _on_back_button_down()
+	{
+		Visible=false;
+		var gameScene=GD.Load<PackedScene>("res://scene/main_menu.tscn");
+		var gameScenebackNode=gameScene.Instantiate();
+		GetParent().AddChild(gameScenebackNode);
+	}
+}
