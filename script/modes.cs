a8b31642bfb72849cffc3c7ffb007163dcd1cceb
matthieu.greiner <matthieu.greiner@epita.fr>
2024-03-18 01:56:45 +0100

    ekeke

--- script/modes.cs
+++ script/modes.cs
@@ -1,55 +1,42 @@
 using Godot;
 using System;
 
-public partial class main_menu : Control
-{
-	[Export] Control variable;
+public partial class modes : Control
+{ 
 	[Export] private AudioStreamPlayer backgroundMusic;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		if (backgroundMusic != null)
-		{
-			backgroundMusic.Play();
-		}
+
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if(Input.IsActionJustPressed("ui_cancel")){
-			variable.Visible=true;
-			
-		}
 	}
-	
-	public void OnplayPressed()
+
+	private void OnSoloPressed()
 	{
-		variable.Visible=false;
-		GetTree().Paused=false;
-		GetNode<Button>("VBoxContainer/Play").Text="Resume";
+		if (backgroundMusic != null && backgroundMusic.Playing)
+		{
+			backgroundMusic.Stop();
+			GD.Print("Musique de fond arrêtée.");
+		}
+
+		Visible=false;
+		GetTree().Paused = false;
 		var mainchildren=GetParent().GetChildren();
 		foreach(var child in mainchildren){
 			if(child.Name=="Tuto"){
 				return;
 			}
 		}
-		var gameScene=GD.Load<PackedScene>("res://scene/modes.tscn");
+		var gameScene=GD.Load<PackedScene>("res://scene/tuto.tscn");
 		var gameSceneNode=gameScene.Instantiate();
 		GetParent().AddChild(gameSceneNode);
 	}
 	
-	public void OnOptionsPressed()
-	{
-		Visible= false;
-		var optionscene=GD.Load<PackedScene>("res://scene/Options.tscn");
-		var optionssceneNode=optionscene.Instantiate();
-		GetParent().AddChild(optionssceneNode);
-	}
-	public void OnExitPressed()
-	{
-		GetTree().Quit();
-	}
 	private void OnBackPressed()
 	{
 		Visible=false;
@@ -58,5 +45,4 @@ public partial class main_menu : Control
 		GetParent().AddChild(gameScenebackNode);
 		
 	}
-	}
-
+}
