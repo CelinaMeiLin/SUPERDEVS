66224d349ca95f438901fe5227549b47da7c40ab
Matthieu GREINER <matthieu.greiner@epita.fr>
2024-04-27 17:51:49 +0200

    ekekekeke

--- script/main_menu.cs
+++ script/main_menu.cs
@@ -3,62 +3,62 @@ using System;
 
 public partial class main_menu : Control
 {
-    [Export] Control variable;
-    [Export] private AudioStreamPlayer backgroundMusic;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        if (backgroundMusic != null)
-        {
-            backgroundMusic.Play();
-        }
-    }
+	[Export] Control variable;
+	[Export] private AudioStreamPlayer backgroundMusic;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		if (backgroundMusic != null)
+		{
+			backgroundMusic.Play();
+		}
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        if(Input.IsActionJustPressed("ui_cancel")){
-            variable.Visible=true;
-            Input.MouseMode = Input.MouseModeEnum.Visible;
-        }
-    }
-    
-    public void OnplayPressed()
-    {
-        variable.Visible=false;
-        GetTree().Paused=false;
-        GetNode<Button>("VBoxContainer/Play").Text="Resume";
-        var mainchildren=GetParent().GetChildren();
-        foreach(var child in mainchildren){
-            if(child.Name=="Tuto"){
-                return;
-            }
-        }
-        GetTree().ChangeSceneToFile("res://scene/modes.tscn");
-        //var gameScene=GD.Load<PackedScene>("res://scene/modes.tscn");
-        //var gameSceneNode=gameScene.Instantiate();
-        //GetParent().AddChild(gameSceneNode);
-    }
-    
-    public void OnOptionsPressed()
-    {
-        Visible= false;
-        GetTree().ChangeSceneToFile("res://scene/Options.tscn");
-        //var optionscene=GD.Load<PackedScene>("res://scene/Options.tscn");
-        //var optionssceneNode=optionscene.Instantiate();
-        //GetParent().AddChild(optionssceneNode);
-    }
-    public void OnExitPressed()
-    {
-        GetTree().Quit();
-    }
-    private void OnBackPressed()
-    {
-        Visible=false;
-        GetTree().ChangeSceneToFile("res://scene/main_menu.tscn");
-        //var gameScene=GD.Load<PackedScene>("res://scene/main_menu.tscn");
-        //var gameScenebackNode=gameScene.Instantiate();
-        //GetParent().AddChild(gameScenebackNode);
-        
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if(Input.IsActionJustPressed("ui_cancel")){
+			variable.Visible=true;
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
+	}
+	
+	public void OnplayPressed()
+	{
+		variable.Visible=false;
+		GetTree().Paused=false;
+		GetNode<Button>("VBoxContainer/Play").Text="Resume";
+		var mainchildren=GetParent().GetChildren();
+		foreach(var child in mainchildren){
+			if(child.Name=="Tuto"){
+				return;
+			}
+		}
+		GetTree().ChangeSceneToFile("res://scene/modes.tscn");
+		//var gameScene=GD.Load<PackedScene>("res://scene/modes.tscn");
+		//var gameSceneNode=gameScene.Instantiate();
+		//GetParent().AddChild(gameSceneNode);
+	}
+	
+	public void OnOptionsPressed()
+	{
+		Visible= false;
+		GetTree().ChangeSceneToFile("res://scene/Options.tscn");
+		//var optionscene=GD.Load<PackedScene>("res://scene/Options.tscn");
+		//var optionssceneNode=optionscene.Instantiate();
+		//GetParent().AddChild(optionssceneNode);
+	}
+	public void OnExitPressed()
+	{
+		GetTree().Quit();
+	}
+	private void OnBackPressed()
+	{
+		Visible=false;
+		GetTree().ChangeSceneToFile("res://scene/main_menu.tscn");
+		//var gameScene=GD.Load<PackedScene>("res://scene/main_menu.tscn");
+		//var gameScenebackNode=gameScene.Instantiate();
+		//GetParent().AddChild(gameScenebackNode);
+		
+	}
 }
--- script/portail.cs
+++ script/portail.cs
@@ -3,28 +3,29 @@ using System;
 
 public partial class portail : Sprite2D
 {
-    
-    private CollisionShape2D collision_shape;
+	
+	private CollisionShape2D collision_shape;
 
-    public override void _Ready()
-    {
-        collision_shape = GetNode<CollisionShape2D>("Area2D/CollisionShape2D");
-       
-    }
+	public override void _Ready()
+	{
+		collision_shape = GetNode<CollisionShape2D>("Area2D/CollisionShape2D");
+	   
+	}
 
-    public void _on_area_2d_body_entered(Node body)
-    {
-        if (body is astra)
-        {
-            //GetTree().CallDeferred("quit");
-            GetTree().ChangeSceneToFile("res://scene/map.tscn");
-        }
-    }
-    
-    
-    public override void _Process(double delta)
-    {
-    }
+	public void _on_area_2d_body_entered(Node body)
+	{
+		if (body is astra)
+		{
+			//GetTree().CallDeferred("quit");
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+			GetTree().ChangeSceneToFile("res://scene/map.tscn");
+		}
+	}
+	
+	
+	public override void _Process(double delta)
+	{
+	}
 }
 
 
