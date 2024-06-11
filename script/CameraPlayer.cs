045c85657183469dfacce64ea6c2e234505eb98e
Dina DIDOUCHE <dina.didouche@epita.fr>
2024-06-11 16:21:12 +0200

    FIXCAMÉRADROITE

--- script/CameraPlayer.cs
+++ script/CameraPlayer.cs
@@ -4,9 +4,13 @@ using System;
 public partial class CameraPlayer : Camera2D
 {
 	[Export] private CharacterBody2D ObjectToFollow;
+	
+	public Node2D RBorderCam;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		RBorderCam = GetParent().GetParent().GetParent().GetParent().GetNode<Node2D>("RBorderLimit");
+		LimitRight = (int)RBorderCam.Position.X;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
